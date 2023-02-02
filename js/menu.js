d4cd9e403a7e5907b27e0c9c47f689865d295aed
9Alla9 <konfetcka@gmail.com>
2023-02-03 00:17:31 +0200

    задание №8

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-Menu-open]"),
+    closeMenuBtn: document.querySelector("[data-Menu-close]"),
+    Menu: document.querySelector("[data-Menu]"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.Menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
